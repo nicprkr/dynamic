329c42143e65ac527ee634b231035a2bdf2276b2
Nicola Parker <a01002393@se14l0124-07.ad.bcit.ca>
2018-01-30 14:52:36 -0800

    added files

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+//asks server to find the public port and use that, ELSE port 3000
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, resp){
+        resp.end("Welcome to my app");
+        });
+
+app.listen(port, (err)=>{
+          if(err){
+        console.log(err);
+        return false;
+          }
+    console.log("port is running!");
+})
