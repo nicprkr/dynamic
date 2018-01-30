18195091d9a1a36fa0b92b48f94523b8fdc0dd50
Nicola Parker <a01002393@se14l0124-07.ad.bcit.ca>
2018-01-30 14:53:59 -0800

    fixed error:

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 //asks server to find the public port and use that, ELSE port 3000
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, resp){
+app.get("/", (req, resp)=>{
         resp.end("Welcome to my app");
         });
 
@@ -13,4 +13,4 @@ app.listen(port, (err)=>{
         return false;
           }
     console.log("port is running!");
-})
+});
